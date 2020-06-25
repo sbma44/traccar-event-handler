38da5ebbe003328de5cba4f1442ec5461f3cf473
Tom Lee <thomas.j.lee@gmail.com>
2020-06-24 23:01:11 -0400

    static images plug traccar config

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from http.server import BaseHTTPRequestHandler
+import os
+import os.path
 import time
+import requests
 from urllib import parse
 from urllib.parse import unquote, urlparse, parse_qs
+from math import radians, cos, sin, asin, sqrt
 import json
 from pushover import Client
 
@@ -10,10 +14,56 @@ try:
 except:
     pass
 
-first_stop = None
+def fetch_static_map(lon, lat):
+    try:
+        url = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/{},{},16,0/300x300?access_token={}'.format(lon, lat, MAPBOX_ACCESS_TOKEN)
+        r = requests.get(url, allow_redirects=True)
+        filename = '/tmp/traccar-map-{}.png'.format(time.time())
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+        
+        # clean up old snaps
+        for fn in os.listdir('/tmp'):
+            if not 'traccar-map-' in fn:
+                continue
+            t = float(os.path.basename(fn).replace('traccar-map-','').replace('.png',''))
+            if (time.time() - t) > 15 * 60:
+                os.unlink(fn)
+
+        return filename
+
+    except:
+        return False
+
+def haversine(e1, e2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    lon1 = float(e1.get('longitude'))
+    lat1 = float(e1.get('latitude'))
+    lon2 = float(e2.get('longitude'))
+    lat2 = float(e2.get('latitude'))
+
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
 
 pushover_client = Client(PUSHOVER_USER, api_token=PUSHOVER_TOKEN)
-pushover_client.send_message('traccar event handler started')
+
+fn = fetch_static_map(-77.0366, 38.8976)
+if fn:
+    with open(fn, 'rb') as f:
+        pushover_client.send_message('traccar event handler started', attachment=f)
+else:
+    pushover_client.send_message('traccar event handler started (maps not working)')
 
 def decode_GET(qs):
     d = parse_qs(qs)
@@ -30,17 +80,55 @@ def decode_GET(qs):
     return new_d
 
 class GetHandler(BaseHTTPRequestHandler):
+    traccar_events = []
+    traccar_state = 'STOPPED'
 
     def do_GET(self):
-        global first_stop
         msg = decode_GET(self.path)
-        if msg.get('speed_mph') > 3:
-            # if we've been stopped for >5m, send a push message
-            if first_stop is not None and time.time() - first_stop > 300:
-                client.send_message('motion detected (DIY)')
-            first_stop = None
-        elif first_stop is None:
-            first_stop = time.time()
+        GetHandler.traccar_events.append(msg)
+        if len(GetHandler.traccar_events) > 100:
+            GetHandler.traccar_events.pop(0)
+
+        if msg.get('speed_mph', 0) > 3:
+            print('speed > 3 mph, checking if car was stopped')
+            if GetHandler.traccar_state == 'STOPPED':
+                sent_map = False
+                if 'latitude' in msg and 'longitude' in msg:
+                    fn = fetch_static_map(msg['longitude'], msg['latitude'])
+                    if fn:
+                        with open(fn, 'rb') as f:
+                            client.send_message('car is moving', attachment=f)
+                            sent_map = True
+
+                if not sent_map:
+                    client.send_message('car is moving')
+
+                GetHandler.traccar_state = 'MOVING'
+        else:
+            # get events from last 3m
+            eligible_events = [e for e in GetHandler.traccar_events if (time.time() - float(e.get('deviceTime', 0))/1000) < 180]
+
+            # find max distance traveled during this period
+            last_event = eligible_events[-1]
+            max_distance = 0
+            for e in eligible_events:
+                max_distance = max(max_distance, haversine(last_event, e))
+
+            print('speed < 3 mph, checking if car has been still -- looks like it moved {} km over {} events'.format(max_distance, len(eligible_events)))
+            if max_distance <= 0.005: # 5 meters
+                if GetHandler.traccar_state == 'MOVING':
+                    sent_map = False
+                    if 'latitude' in msg and 'longitude' in msg:
+                        fn = fetch_static_map(msg['longitude'], msg['latitude'])
+                        if fn:
+                            with open(fn, 'rb') as f:
+                                client.send_message('car is stopped', attachment=f)
+                                sent_map = True
+
+                    if not sent_map:
+                        client.send_message('car is stopped')
+
+                    GetHandler.traccar_state = 'STOPPED'
 
         with open('GET.log', 'a') as f:
             f.write(str(time.time()) + ',' + json.dumps(msg) + '\n')
@@ -66,8 +154,6 @@ class GetHandler(BaseHTTPRequestHandler):
         self.send_header("Content-Length", "2")
         self.end_headers()
         self.wfile.write("OK".encode("utf-8"))
-
-
 
 if __name__ == '__main__':
     from http.server import HTTPServer
